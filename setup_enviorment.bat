4e05257c0e900b3df967da8f6dd547389042aaf7
complay11 <84672664+complay11@users.noreply.github.com>
2024-05-26 18:47:24 +0200

    helpful file for creating venvs
    venvs are too large to upload to source control (3GB+) so this file does that for you

---
+++ setup_enviorment.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Setting up virtual enviorments, please wait...
+echo Creating venv for gui-p
+cd gui-p
+python -m venv .venv
+.venv\Scripts\pip.exe install requests customtkinter
+echo Creating venv for daemon
+cd ..
+cd daemon
+python -m venv .venv
+.venv\Scripts\pip.exe install dotenv transformers fastapi pydantic uvicorn
+.venv\Scripts\pip.exe install torch --index-url https://download.pytorch.org/whl/cu121
+echo HF_TOKEN=YOUR_HUGGING_FACE_TOKEN > .env
+echo HF_HUB=./model/ >> .env
