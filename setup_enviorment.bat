f3606a95e9ba256d8e508c335507340726ae1683
complay11 <84672664+complay11@users.noreply.github.com>
2024-05-26 19:01:32 +0200

    bandaid fix

--- setup_enviorment.bat
+++ setup_enviorment.bat
@@ -8,7 +8,7 @@ echo Creating venv for daemon
 cd ..
 cd daemon
 python -m venv .venv
-.venv\Scripts\pip.exe install dotenv transformers fastapi pydantic uvicorn
+.venv\Scripts\pip.exe install dotenv-python transformers fastapi pydantic uvicorn
 .venv\Scripts\pip.exe install torch --index-url https://download.pytorch.org/whl/cu121
 echo HF_TOKEN=YOUR_HUGGING_FACE_TOKEN > .env
 echo HF_HUB=./model/ >> .env
